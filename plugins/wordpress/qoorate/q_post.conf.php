9adbe6be3ae3fed8e967b8ecd1825616f804b37b
Seth Murphy <seth@sethmurphy.com>
2012-08-27 23:47:29 -0300

    Update plugins/wordpress/qoorate/q_post.conf.php
    fixed QOORATE_UPLOADER_URI to have q prefix: http://qrate.co/q/uploader

--- plugins/wordpress/qoorate/q_post.conf.php
+++ plugins/wordpress/qoorate/q_post.conf.php
@@ -1,7 +1,7 @@
 <?php
 // These are the paths to the API server that the proxy needs
 define('QOORATE_BASE_URI', 'http://qrate.co/q');
-define('QOORATE_UPLOADER_URI', 'http://qrate.co/uploader');
+define('QOORATE_UPLOADER_URI', 'http://qrate.co/q/uploader');
 define('QOORATE_FEED_URI', 'http://qrate.co/q/feed');
 define('QOORATE_EMBED_URI', 'http://qrate.co/q/embed');
 define('QOORATE_DEBUG', False);
